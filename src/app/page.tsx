f882c574c0ec42b07413cfe2649b800246147c8d
barnilsarma <barnilsarma@gmail.com>
2024-09-10 18:24:54 +0530

    checks on device optimization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,14 +15,35 @@ export default function HomePage() {
     setIsClient(true)
   }, [])
   const matches = useMediaQuery("(max-width: 1024px)")
-  return (
-    <main className="bg-black">
-      <Navbar />
-      {isClient && matches && <NavbarMobile />}
-      <div className="h-screen">
-        <Scene />
-      </div>
-      {/* <Footer /> */}
-    </main>
-  );
+  let browser=/Mozilla/i;
+  let isBrowserMozilla=browser.test(navigator.userAgent);
+  console.log(isBrowserMozilla);
+  if(!isBrowserMozilla){
+    return (
+      <main className="bg-black">
+        <Navbar />
+        {isClient && matches && <NavbarMobile />}
+        <div className="h-screen">
+          {
+            navigator.deviceMemory<=2 && !(/Android/i.test(navigator.userAgent))?
+              <h1>Android</h1>:<Scene />
+          }
+          {/* <Scene /> */}
+        </div>
+        {/* <Footer /> */}
+      </main>
+    );
+  }
+  else{
+    return (
+      <main className="bg-black">
+        <Navbar />
+        {isClient && matches && <NavbarMobile />}
+        <div className="h-screen">
+          {/* <Scene /> */}
+        </div>
+        {/* <Footer /> */}
+      </main>
+    );
+  }
 }
