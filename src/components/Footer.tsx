5ea720f289063630d8cfbded714c650db96c3507
tech-hunter-mainak <153637016+tech-hunter-mainak@users.noreply.github.com>
2024-10-02 20:35:17 +0530

    small changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,23 +10,27 @@ const Footer = () => {
   const glow = document.querySelector(".glow") as HTMLElement;
 
   document.addEventListener("mousemove", (e) => {
-    (glow) ? glow.style.transform = `translate(${e.pageX - 50}px, ${e.pageY - 50}px)` : '';
+    glow
+      ? (glow.style.transform = `translate(${e.pageX - 50}px, ${e.pageY - 50}px)`)
+      : "";
   });
 
   return (
     <footer className="fixed left-0 top-4 z-10 flex w-full items-center justify-center p-1 text-2xl lg:text-3xl">
       <div className="glow"></div>
       <div className="back-cover">
-        <div className="tecno-big-img"></div>
-        <div className="bottom-content">
-          <span className="social-links">
-            <Link href={"https://www.instagram.com/tecnoesis.nits/"}>
-              <IoLogoInstagram className="hover:text-gray-200" />
-            </Link>
-            <Link href={"https://www.facebook.com/tecnoesis.nits"}>
-              <IoLogoFacebook className="hover:text-gray-200" />
-            </Link>
-          </span>
+        <div className="star-background">
+          <div className="tecno-big-img"></div>
+          <div className="bottom-content">
+            <span className="social-links">
+              <Link href={"https://www.instagram.com/tecnoesis.nits/"}>
+                <IoLogoInstagram className="hover:text-gray-200" />
+              </Link>
+              <Link href={"https://www.facebook.com/tecnoesis.nits"}>
+                <IoLogoFacebook className="hover:text-gray-200" />
+              </Link>
+            </span>
+          </div>
         </div>
       </div>
     </footer>
