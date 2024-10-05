cbe74dcc03bd654a63fef04db9d3b514a0dbc9ac
velgardey <deymriganka2004@gmail.com>
2024-10-05 13:23:43 +0530

    removes temp files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 "use client";
-import dynamic from "next/dynamic";
-import { useEffect, useState } from "react";
-import { useMediaQuery } from "usehooks-ts";
 import Login from "~/components/GoogleAuth";
 import Landing from "~/components/Landing";
 import Navbar from "~/components/LandingNav";
 import Scene from "~/components/Scene";
+import { useMediaQuery } from "usehooks-ts";
+import dynamic from 'next/dynamic';
+import { useEffect, useState } from "react";
+import LandingFooter from "~/components/LandingFooter";
 
 const NavbarMobile = dynamic(() => import("~/components/LandingNavMobile"));
 
@@ -17,53 +18,17 @@ export default function HomePage() {
   const [isClient, setIsClient] = useState(false);
   const comingsoon = false;
   useEffect(() => {
-    setIsClient(true);
-  }, []);
-
-  const matches = useMediaQuery("(max-width: 1024px)");
-
-  if (!isClient) return null;
-  const nav = navigator as NavigatorExtended;
-  const isFirefox = navigator.userAgent.includes("Firefox");
-  const isLowMemoryDevice = nav.deviceMemory! <= 4;
-  const isAndroid = /Android/i.test(navigator.userAgent);
-  if (comingsoon) {
-    if (!isFirefox) {
-      return (
-        <main className="bg-black">
-          <Navbar />
-          {matches && <NavbarMobile />}
-          <div className="h-screen">
-            {isLowMemoryDevice && isAndroid ? (
-              <div className="flex min-h-screen items-center justify-center text-[#ffffff]">
-                <p>Low memory Android device detected</p>
-              </div>
-            ) : (
-              <Scene />
-            )}
-            <Login />
-          </div>
-        </main>
-      );
-    } else {
-      return (
-        <main className="bg-black">
-          <Navbar />
-          {matches && <NavbarMobile />}
-          <div className="flex h-screen items-center justify-center">
-            <h1 className="text-[#ffffff]">
-              Please have the mercy to use chromium based browsers
-            </h1>
-            <Login />
-          </div>
-        </main>
-      );
-    }
-  } else {
-    return (
-      <div className="min-h-screen w-screen overflow-hidden bg-[url('/assets/Landing/stars-bg.avif')]">
-        <Landing />
-      </div>
-    );
-  }
+    setIsClient(true)
+  }, [])
+  const matches = useMediaQuery("(max-width: 1024px)")
+  return (
+<main className="bg-black min-h-screen flex flex-col">
+  <Navbar />
+  {isClient && matches && <NavbarMobile />}
+  <div className="flex-grow">
+    <Scene />
+  </div>
+  {/* <Footer /> */}
+</main>
+  );
 }
