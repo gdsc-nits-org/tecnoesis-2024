fb7aadc3def2adc663f2e36b3b3ce3f2061fcc6d
tech-hunter-mainak <153637016+tech-hunter-mainak@users.noreply.github.com>
2024-10-12 06:52:39 +0530

    linting and building issues fixed

--- src/components/FinalFooter.tsx
+++ src/components/FinalFooter.tsx
@@ -124,7 +124,7 @@ const LandingFooter = () => {
         if (particle) {
           particle.draw();
         }
-      }
+      }      
       connectParticles();
       requestAnimationFrame(animateParticles);
     }
