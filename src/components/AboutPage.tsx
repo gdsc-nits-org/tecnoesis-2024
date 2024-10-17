95a59e19fec9b482687827f46e5b0c2c4a205d93
Rishav Bora <Rishavbora01@gmail.com>
2024-10-17 16:29:30 +0530

    animate component

--- src/components/AboutPage.tsx
+++ src/components/AboutPage.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Image from 'next/image';
 import { useEffect, useRef } from 'react';
 import { gsap } from 'gsap';
 import CustomButton from '../components/CustomButton'
@@ -8,11 +7,19 @@ import CustomButton from '../components/CustomButton'
 
 const About = () => {
 
-    const image2WrapperRef = useRef<HTMLDivElement | null>(null);
+    const image2WrapperRef = useRef<HTMLImageElement | null>(null);
     const image1Ref = useRef<HTMLImageElement | null>(null);
     const image2Ref = useRef<HTMLImageElement | null>(null);
     const image3Ref = useRef<HTMLImageElement | null>(null);
     const class1DivRef = useRef<HTMLDivElement | null>(null);
+    const imageARef = useRef<HTMLImageElement | null>(null);
+    const imageCRef = useRef<HTMLImageElement | null>(null);
+    const imageDRef = useRef<HTMLImageElement | null>(null);
+    const imageERef = useRef<HTMLImageElement | null>(null);
+    const imageFRef = useRef<HTMLImageElement | null>(null);
+    const imageGRef = useRef<HTMLImageElement | null>(null);
+
+
 
 
     useEffect(() => {
@@ -25,13 +32,73 @@ const About = () => {
                 ease: "power1.inOut",
             });
         }
-    }, []);
+
+    }
+
+        , []);
 
     useEffect(() => {
         const handleMouseMove = (event: MouseEvent) => {
             const mouseX = event.clientX;
             const mouseY = event.clientY;
 
+            if (imageARef.current) {
+                gsap.to(imageARef.current, {
+                    x: (mouseX - window.innerWidth / 2) * 0.1,
+                    y: (mouseY - window.innerHeight / 2) * 0.1,
+                    duration: 0.6,
+                    ease: "power3.out",
+                });
+            }
+
+            if (image2WrapperRef.current) {
+                gsap.to(image2WrapperRef.current, {
+                    x: (mouseX - window.innerWidth / 2) * 0.09,
+                    y: (mouseY - window.innerHeight / 2) * 0.09,
+                    duration: 0.6,
+                    ease: "power3.out",
+                });
+            }
+            if (imageCRef.current) {
+                gsap.to(imageCRef.current, {
+                    x: (mouseX - window.innerWidth / 2) * 0.07,
+                    y: (mouseY - window.innerHeight / 2) * 0.07,
+                    duration: 0.6,
+                    ease: "power3.out",
+                });
+            }
+            if (imageDRef.current) {
+                gsap.to(imageDRef.current, {
+                    x: (mouseX - window.innerWidth / 2) * 0.065,
+                    y: (mouseY - window.innerHeight / 2) * 0.065,
+                    duration: 0.6,
+                    ease: "power3.out",
+                });
+            }
+            if (imageERef.current) {
+                gsap.to(imageERef.current, {
+                    x: (mouseX - window.innerWidth / 2) * 0.06,
+                    y: (mouseY - window.innerHeight / 2) * 0.06,
+                    duration: 0.6,
+                    ease: "power3.out",
+                });
+            }
+            if (imageFRef.current) {
+                gsap.to(imageFRef.current, {
+                    x: (mouseX - window.innerWidth / 2) * 0.06,
+                    y: (mouseY - window.innerHeight / 2) * 0.06,
+                    duration: 0.6,
+                    ease: "power3.out",
+                });
+            }
+            if (imageGRef.current) {
+                gsap.to(imageGRef.current, {
+                    x: (mouseX - window.innerWidth / 2) * 0.05,
+                    y: (mouseY - window.innerHeight / 2) * 0.05,
+                    duration: 0.6,
+                    ease: "power3.out",
+                });
+            }
 
             if (image1Ref.current) {
                 gsap.to(image1Ref.current, {
@@ -90,64 +157,48 @@ const About = () => {
                     <div className="px-7 text-[#B5D8EABF] text-base text-justify pb-4 font-outfit lg:text-left md:text-center md:text-3xl lg:text-xl">Tecnoesis is the annual techno-managerial event of NIT Silchar, promising all tech geeks the ideal niche of fascinating events, workshops, competitions and interactions worth a lifetime. This mega event has left its mark as of the most prominent techfests across the country. presenting the chance to let their minds run wild with ideas, fostering the inventors of the future.</div>
                 </div>
                 <div className="lower w-full h-[40vh] flex justify-center items-center lg:w-1/3">
-                    <div className="relative w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] overflow-visible ">
-                        <Image
+                    <div className="first-animation relative w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] overflow-visible ">
+                        <img ref={imageARef}
                             src="https://res.cloudinary.com/dxafdfvui/image/upload/v1728465500/1_oztzxp.webp"
-                            alt="Image 1"
+                            alt="Image a"
                             className="absolute top-1/2 left-1/2 w-[30%] h-[30%] transform -translate-x-1/2 -translate-y-1/2 z-7"
-                            width={250}
-                            height={250}
                             style={{ objectFit: 'cover' }}
                         />
-                        <div ref={image2WrapperRef} className="absolute top-1/2 left-1/2 w-[80%] h-[80%] lg:w-[60%] lg:h-[60%] transform -translate-x-1/2 -translate-y-1/2 z-6">
-                            <Image
-                                src="https://res.cloudinary.com/dxafdfvui/image/upload/v1728465506/2_fb0wb7.webp"
-                                alt="Image 2"
-                                width={250}
-                                height={250}
-                                style={{ objectFit: 'cover' }}
-                            />
-                        </div>
+                        <img
+                            ref={image2WrapperRef}
 
-                        <Image
+                            src="https://res.cloudinary.com/dxafdfvui/image/upload/v1728465506/2_fb0wb7.webp"
+                            alt="Image b"
+                            className="absolute top-1/2 left-1/2 w-[80%] h-[80%]  lg:w-[60%] lg:h-[60%] transform -translate-x-1/2 -translate-y-1/2 z-6"
+                            style={{ objectFit: 'cover' }}
+                        />
+                        <img ref={imageCRef}
                             src="https://res.cloudinary.com/dxafdfvui/image/upload/v1728465506/Flow_7_n6z8tt.webp"
-                            alt="Image 3"
+                            alt="Image c"
                             className="absolute top-1/2 left-1/2 w-[65%] h-[65%] transform -translate-x-1/2 -translate-y-1/2 z-5"
-                            width={250}
-                            height={250}
-                            style={{ objectFit: 'cover' }}
                         />
-                        <Image
+                        <img ref={imageDRef}
                             src="https://res.cloudinary.com/dxafdfvui/image/upload/v1728465500/dotted_ring_tcdt3j.webp"
-                            alt="Image 4"
+                            alt="Image d"
                             className="absolute top-1/2 left-1/2 w-[70%] h-[70%] transform -translate-x-1/2 -translate-y-1/2 z-4"
-                            width={250}
-                            height={250}
-                            style={{ objectFit: 'cover' }}
                         />
-                        <Image
+                        <img ref={imageERef}
                             src="https://res.cloudinary.com/dxafdfvui/image/upload/v1728465506/static_ring_ehsm66.webp"
-                            alt="Image 5"
+                            alt="Image e"
                             className="absolute top-1/2 left-1/2 w-full h-full transform -translate-x-1/2 -translate-y-1/2 z-3 object-contain scale-[1.35]"
-                            width={250}
-                            height={250}
                         />
-                        <Image
+                        <img ref={imageFRef}
                             src="https://res.cloudinary.com/dxafdfvui/image/upload/v1728465501/Flow_6_1_ladpab.webp"
-                            alt="Image 6"
+                            alt="Image f"
                             className="absolute top-1/2 left-1/2 w-[100%] h-[100%] transform -translate-x-1/2 -translate-y-1/2 z-2"
-                            width={250}
-                            height={250}
-                            style={{ objectFit: 'cover' }}
                         />
-                        <Image
+                        <img ref={imageGRef}
                             src="https://res.cloudinary.com/dxafdfvui/image/upload/v1728465507/outer_ring_amzrc1.webp"
-                            alt="Image 7"
+                            alt="Image g"
                             className="absolute top-1/2 left-1/2 w-full h-full transform -translate-x-1/2 -translate-y-1/2 z-1 scale-[1.5] object-contain"
-                            width={250}
-                            height={250}
                         />
                     </div>
+
                 </div>
                 <div className="w-full h-1/2 lg:w-1/3 lg:h-auto">
                     <div className="p-7 text-gradient-blue  font-rp1 text-2xl font-bold md:text-5xl text-center lg:text-right md:m-0">ABOUT US</div>
@@ -177,7 +228,7 @@ const About = () => {
 
                 </div>
                 <div className="w-full h-[60vh]  md:h-[100%]  overflow-hidden flex flex-col justify-center">
-                    <div className="relative w-full h-[300px] md:h-[500px]  top-[-60px] ">
+                    <div className="second-animation relative w-full h-[300px] md:h-[500px]  top-[-60px] ">
 
                         <div ref={class1DivRef} className="class1 rounded-3xl absolute top-1/2 w-[320px] md:w-[440px] left-1/2 transform -translate-x-1/2 h-1/2 flex items-center justify-center inset-0 bg-[url('https://res.cloudinary.com/dxafdfvui/image/upload/v1728624347/a33f887a52eced78d356c06b600b05c8_chf68q.webp')] bg-cover bg-center opacity-50 z-3">
                         </div>
