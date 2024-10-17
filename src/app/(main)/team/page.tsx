69616b41ece45faf80dfe7ef8a9640fab460d5e0
Arindom <arindom956@gmail.com>
2024-10-18 00:53:53 +0530

    Team page modification

--- src/app/(main)/team/page.tsx
+++ src/app/(main)/team/page.tsx
@@ -1,13 +1,14 @@
 "use client"
 
 import Card, { MemberCard } from '~/components/Card';
-import data from "../../../public/team.json";
+import data from "../../../../public/team.json";
 import Marquee from 'react-fast-marquee';
 import { useState } from 'react';
 
 export const runtime = "edge";
 export default function Team() {
-    const [team, setTeam] = useState<1|2>(1);
+    const [team, setTeam] = useState<1|2|3>(1);
+    const [hovers,setHovers]=useState(true);
      return (
         <div className='min-h-screen'>
             <div className="bg-customBluish min-h-screen">
@@ -18,13 +19,18 @@ export default function Team() {
                     <button style={team==1?{backgroundColor:"#59CAFA"}:{}} onClick={()=>{
                         setTeam(1);
                     }} className='group bg-[#fff1] flex items-center justify-between rounded-full px-[2vw] py-[0.75vw] shadow-[inset_1px_2px_2.5px_rgba(255,255,255,0.3),inset_1px_-2px_2.5px_rgba(255,255,255,0.3)] duration-1000 hover:shadow-[inset_1px_2px_2.5px_rgba(1,163,245,0.5),inset_1px_-2px_2.5px_rgba(1,163,245,0.5)]'>
-                        <p className="font-outfit group-hover:text-customBlue group-hover:scale-90 duration-300">Tech Team</p>
+                        <p className="font-outfit group-hover:text-customBlue group-hover:scale-90 duration-300">Core Team</p>
                     </button>
                     <button style={team==2?{backgroundColor:"#59CAFA"}:{}} onClick={()=>{
                         setTeam(2);
                     }} className='group bg-[#fff1] flex items-center justify-between rounded-full px-[2vw] py-[0.75vw] shadow-[inset_1px_2px_2.5px_rgba(255,255,255,0.3),inset_1px_-2px_2.5px_rgba(255,255,255,0.3)] duration-1000 hover:shadow-[inset_1px_2px_2.5px_rgba(1,163,245,0.5),inset_1px_-2px_2.5px_rgba(1,163,245,0.5)]'>
-                        <p className= "font-outfit group-hover:text-customBlue group-hover:scale-90 duration-300">Other Team</p>
+                        <p className= "font-outfit group-hover:text-customBlue group-hover:scale-90 duration-300">Tech Team</p>
                     </button>
+                     <button style={team == 3 ? { backgroundColor: "#59CAFA" } : {}} onClick={() => {
+                         setTeam(3);
+                     }} className='group bg-[#fff1] flex items-center justify-between rounded-full px-[2vw] py-[0.75vw] shadow-[inset_1px_2px_2.5px_rgba(255,255,255,0.3),inset_1px_-2px_2.5px_rgba(255,255,255,0.3)] duration-1000 hover:shadow-[inset_1px_2px_2.5px_rgba(1,163,245,0.5),inset_1px_-2px_2.5px_rgba(1,163,245,0.5)]'>
+                         <p className="font-outfit group-hover:text-customBlue group-hover:scale-90 duration-300">Module Heads</p>
+                     </button>
                 </div>
                 <div className='flex flex-col justify-center items-center text-yellow-50'>
                     {
@@ -47,13 +53,13 @@ export default function Team() {
                     <Marquee
                         speed={70}
                         direction={"left"}
-                        pauseOnClick={true}
+                        play={hovers}
                     >
                         <div className='flex h-[40rem]'>
                             {
                                 data.map((item) => (
                                     item.members.map((member) => (
-                                        <MemberCard key={member.id} name={member.name} designation={member.designation} photo={member.photo} index={parseInt(member.id)} facebook={member.facebook ?? ""} instagram={member.instagram ?? ""} linkedin={member.linkedin ?? ""} />
+                                        <MemberCard key={member.id} name={member.name} designation={member.designation} photo={member.photo} index={parseInt(member.id)} facebook={member.facebook ?? ""} instagram={member.instagram ?? ""} linkedin={member.linkedin ?? ""} hoversetter={setHovers} />
                                     ))
                                 ))
                             }
