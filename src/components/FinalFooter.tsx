bf23b3d4be1f0472188f07a4792028b1dbaf7dba
tech-hunter-mainak <153637016+tech-hunter-mainak@users.noreply.github.com>
2024-10-20 22:13:29 +0530

    changed footer

--- src/components/FinalFooter.tsx
+++ src/components/FinalFooter.tsx
@@ -158,13 +158,13 @@ const LandingFooter = () => {
     <>
       <footer className="relative flex min-h-[50vh] w-full flex-col overflow-hidden bg-black">
         <div
-          className="bg-[linear-gradient(black, #000000)] z-50 mb-[-150px] h-[180px] w-full"
+          className="bg-[linear-gradient(black, #000000)] z-40 mb-[-150px] h-[160px] w-full"
           style={{
             background:
               "linear-gradient(#000000, #0000009e, rgb(0 0 0 / 33%), #00000000)",
           }}
         ></div>
-        <div className="inset-0 z-0 w-full">
+        <div className="inset-0 z-0 w-auto">
           <canvas
             ref={canvasRef}
             style={{
@@ -179,18 +179,18 @@ const LandingFooter = () => {
         {mouseInsideFooter && !isMobile ? (
           <div
             ref={glowRef}
-            className="glow duration-[50ms] pointer-events-none absolute left-0 top-0 z-30 h-[200px] w-[300px] scale-[2] rounded-full bg-[radial-gradient(circle,_rgba(255,255,255,0.212),_rgba(255,255,255,0.203),_rgba(255,255,255,0.112),_rgba(255,255,255,0.057),_rgba(255,255,255,0.018),_rgba(255,255,255,0),_rgba(255,255,255,0))] bg-scroll opacity-50 bg-blend-difference shadow-[0_0_15px_10px_rgba(255,255,255,0.016)] transition-transform ease-linear"
+            className="glow duration-[50ms] pointer-events-none absolute left-0 top-0 z-30 h-[300px] w-[300px] scale-[2] rounded-full bg-[radial-gradient(circle,_rgba(255,255,255,0.212),_rgba(255,255,255,0.203),_rgba(255,255,255,0.112),_rgba(255,255,255,0.057),_rgba(255,255,255,0.018),_rgba(255,255,255,0),_rgba(255,255,255,0))] bg-scroll opacity-50 bg-blend-difference shadow-[0_0_15px_10px_rgba(255,255,255,0.016)] transition-transform ease-linear"
           ></div>
         ) : (
           ""
         )}
-        <div className="back-cover z-20 flex h-fit w-full flex-grow flex-col items-center justify-between bg-[url('/assets/footer/imgs/globe.webp')] bg-cover bg-center">
+        <div className="z-20 flex h-fit w-full flex-grow flex-col items-center justify-between bg-[url('/assets/footer/imgs/globe.webp')] bg-cover bg-center">
           <div className="flex w-full flex-col items-center justify-between">
-            <div className="tecno-big-img mb-0 mt-[150px] flex h-4/6 h-[40vh] h-[50vh] w-9/12 w-full flex-col items-center justify-center bg-[url('/assets/footer/imgs/tecno.webp')] bg-contain bg-center bg-no-repeat sm:h-[30vh] sm:w-[120%] md:h-[35vh] md:w-[110%] lg:h-[50vh] lg:w-full"></div>
+            <div className="mb-0 mt-[150px] flex h-[30vh] w-[90vw] flex-col items-center justify-center bg-[url('/assets/footer/imgs/tecno.webp')] bg-contain bg-center bg-no-repeat sm:h-[30vh] sm:w-[120%] md:h-[35vh] md:w-[110%] lg:h-[40vh] lg:w-full mb-[-20px]"></div>
           </div>
 
-          <div className="mt-auto flex w-full flex-col items-center justify-end py-8">
-            <span className="flex justify-center gap-x-3.5">
+          <div className="mt-auto flex w-full flex-col items-center justify-end py-8 mb-0 sm:mb-[-10px]">
+            <span className="flex justify-center gap-x-3.5 mb-0 sm:mb-[-10px]">
               <Link
                 href={"https://www.instagram.com/tecnoesis.nits/"}
                 aria-label="Instagram"
@@ -213,9 +213,9 @@ const LandingFooter = () => {
                 <FaXTwitter className="text-2xl text-[1.5rem] text-white hover:text-gray-200 sm:text-3xl md:text-4xl md:text-[1.5rem] lg:text-5xl lg:text-[1.5rem] xl:text-[1.5rem]" />
               </Link>
             </span>
-            <div className="flex items-center text-center w-auto pl-[20px] pr-[20px] !text-[1rem] text-[0.875rem] text-sm font-medium leading-[1.5] tracking-[0.15em] tracking-[0.2em] text-white sm:text-base sm:tracking-[0.25em] md:text-lg md:tracking-[0.25em] lg:text-xl lg:tracking-[0.3em] xl:tracking-[0.35em]">
+            <div className="flex items-center text-center w-auto pl-[20px] pr-[20px] !text-[1rem] text-[0.875rem] text-sm font-medium leading-[1.5] tracking-[0.15em] tracking-[0.2em] text-white sm:text-base sm:tracking-[0.25em] md:text-lg md:tracking-[0.25em] lg:text-xl lg:tracking-[0.3em] xl:tracking-[0.35em] mb-[-30px] flex-col sm:flex-row">
               Designed in collaboration with GDG NIT Silchar 
-              <img src="https://res.cloudinary.com/dagggqd6g/image/upload/f_auto,q_auto/hyg5xs9vkkpswxiiidp3" alt="gdgc_logo" className="w-[150px] h-[90px] mt-[22px] ml-[-50px]" />
+              <img src="https://res.cloudinary.com/dagggqd6g/image/upload/f_auto,q_auto/hyg5xs9vkkpswxiiidp3" alt="gdgc_logo" className="w-[150px] h-[90px] sm:mt-[22px] sm:ml-[-50px] sm:mb-[-10px] mb-[-30px]" />
             </div>
           </div>
         </div>
