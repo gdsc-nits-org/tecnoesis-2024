ce17f177f0e74e6fc3e9c3741a1af895f879b3e9
karock <148215838+hirakRajbonshi@users.noreply.github.com>
2024-10-20 19:44:19 +0530

    Nav fix (#41)
    * initial
    
    * navbar mobile
    
    * Bigger Screen Navbar and its Responsiveness
    
    * improvised
    
    * Responsiveness Fixed
    
    * transition added in mobNav
    
    * remove unnecessary marquees
    
    * Fixed production errors
    
    * Lockfile issues
    
    * issues resolved
    
    * minor change
    
    * Delete .vscode directory
    
    * Made necessaary changes except ALIAS
    
    * improved navbar
    
    * fixed build issue
    
    * minor fixes
    
    * fixes
    
    * custom button and navbar ui improved
    
    * minor change
    
    * navbar feature & footer bug fix
    
    * build fix
    
    * Logo and position fixes
    
    * fixed navbar scroll
    
    * Ekdom hyration error fix kori disung aru
    
    ---------
    
    Co-authored-by: A-nirvana <anirban_ug_23@gmail.com>
    Co-authored-by: Pixel Hermit <145558376+A-nirvana@users.noreply.github.com>

--- src/components/AboutPage.tsx
+++ src/components/AboutPage.tsx
@@ -261,7 +261,7 @@ const About = () => {
 
 
     return (
-        <div className="flex flex-col w-screen overflow-x-hidden" >
+        <div className="flex flex-col w-screen overflow-x-hidden" id='about'>
             <div className="flex flex-col lg:flex-row items-center justify-center lg:h-[80vh] md:px-12">
                 <div className=" upper w-full  md:h-[30vh]  lg:w-1/3 lg:h-auto">
                     <div className=" text-gradient-blue p-10 font-rp1 text-2xl font-bold md:text-5xl text-center lg:text-left lg:text-[2.5rem] desktop1:text-[3rem]">ABOUT NIT SILCHAR</div>
