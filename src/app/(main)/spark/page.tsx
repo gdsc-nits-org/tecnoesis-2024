1c028429735499740214958eb0336037f1c31c11
rishinhk004 <rishirajsaha555@gmail.com>
2024-10-25 00:44:13 +0530

    Spark init

--- src/app/(main)/spark/page.tsx
+++ src/app/(main)/spark/page.tsx
@@ -1,9 +1,22 @@
+import Landing from "~/components/Spark/Landing";
+import About from "~/components/Spark/About";
+import Gallery from "~/components/Spark/Gallery";
+
 export const runtime = "edge";
 
-const Spark = () =>{
-    return(
-        <>
-        </>
+const Spark = () => {
+    return (
+        <div className="bg-dotted min-h-screen w-full overflow-hidden flex flex-col items-center justify-center">
+            <div className="h-fit">
+                <Landing />
+            </div>
+            <div className="h-fit">
+                <About />
+            </div>
+            <div className="h-fit">
+                <Gallery />
+            </div>
+        </div>
     );
 }
 
