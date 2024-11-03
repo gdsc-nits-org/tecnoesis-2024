a18adff62193ee66db24e1df5690f9d43e827583
barnilsarma <barnilsarma@gmail.com>
2024-11-03 17:35:57 +0530

    landing start

--- src/app/(main)/spark/page.tsx
+++ src/app/(main)/spark/page.tsx
@@ -8,9 +8,10 @@ export const runtime = "edge";
 const Spark = () => {
   return (
     <div className="bg-dotted flex min-h-screen w-full flex-col items-center justify-center overflow-hidden">
-      {/* <div className="h-fit">
+       <div className="h-fit">
         <Landing />
       </div>
+      {/*
       <div className="mb-[4rem] h-fit lg:mt-[4rem]">
         <About />
       </div>
--- src/components/Spark/Landing.tsx
+++ src/components/Spark/Landing.tsx
@@ -3,11 +3,11 @@ import React from 'react';
 import Glitch from './Glitch';
 const Landing: React.FC = () => {
   return (
-    <div className="sparkheroParent text-white w-[100vw] min-h-[50vh] md:min-h-[100vh] bg-[url('/assets/spark/sparklanding.png')] bg-cover bg-center flex flex-col justify-center items-center relative">
+    <div className="sparkheroParent scale-x-[-1] scale-y-[1] text-white w-[100vw] min-h-[50vh] md:min-h-[100vh] bg-[url('/assets/spark/sparklanding.png')] bg-cover bg-center flex flex-col justify-center items-center relative">
       <div className="flex flex-col items-center justify-center -translate-y-10">
         <div className="flex flex-col items-center justify-center h-[100%] w-[100%]">
           {/* <h1 className='font-rp1 text-4xl md:text-8xl text-white sparkHero'>SPARK</h1> */}
-          <Glitch text="SPARK" />
+          
         </div>
       </div>
     </div>
