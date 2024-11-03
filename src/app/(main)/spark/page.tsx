36e43bc7d142dfca0ab4cc483eca3fb53fe5b7fb
rishinhk004 <rishirajsaha555@gmail.com>
2024-11-04 00:24:06 +0530

    cursor add

--- src/app/(main)/spark/page.tsx
+++ src/app/(main)/spark/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+import { useEffect, useState } from "react";
 import Landing from "~/components/Spark/Landing";
 import About from "~/components/Spark/About";
 import Gallery from "~/components/Spark/Gallery";
@@ -6,8 +8,37 @@ import AboutArtist from "~/components/Spark/AboutArtist";
 export const runtime = "edge";
 
 const Spark = () => {
+  const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const handleMouseMove = (e: MouseEvent) => {
+
+      setCursorPosition({ x: e.clientX, y: e.clientY });
+    };
+    window.addEventListener("mousemove", handleMouseMove);
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
   return (
-    <div className="bg-dotted flex min-h-screen w-full flex-col items-center justify-center overflow-hidden">
+    <div className="bg-dotted flex min-h-screen w-full flex-col items-center justify-center overflow-hidden xl:cursor-none "
+    >
+      <div className="hidden xl:flex"
+        style={{
+          position: "fixed",
+          top: cursorPosition.y,
+          left: cursorPosition.x,
+          width: "10rem",
+          height: "10rem",
+          pointerEvents: "none",
+          backgroundImage: "url('/assets/spark/custom-cursor.gif')",
+          backgroundSize: "cover",
+          transform: "translate(-50%, -50%)",
+          zIndex: 9999,
+        }}
+      />
+
       <div className="h-fit">
         <Landing />
       </div>
@@ -18,7 +49,7 @@ const Spark = () => {
       <div className="mb-[6rem]">
         <AboutArtist />
       </div>
-      <div className=" w-[90vw]">
+      <div className="w-[90vw]">
         <Gallery />
       </div>
       <FinalFooter />
--- src/components/Spark/Landing.tsx
+++ src/components/Spark/Landing.tsx
@@ -1,38 +1,13 @@
 "use client";
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
 const Landing: React.FC = () => {
-    const [position, setPosition] = useState({ x: 0, y: 0 });
-
-    useEffect(() => {
-        const handleMouseMove = (e: MouseEvent) => {
-            setPosition({ x: e.clientX, y: e.clientY });
-        };
-
-        window.addEventListener('mousemove', handleMouseMove);
-        return () => {
-            window.removeEventListener('mousemove', handleMouseMove);
-        };
-        
-    }, []);
-
     return (
         <div className="sparkheroParent scale-x-[-1] text-white w-[100vw] min-h-[50vh] md:min-h-[100vh] bg-[url('/assets/spark/sparklanding.png')] bg-cover bg-center flex flex-col justify-center items-center relative">
             <div
-                style={{
-                    position: 'absolute',
-                    left: position.x,
-                    top: position.y,
-                    width: '32px',
-                    height: '32px',
-                    background: 'url(/assets/spark/custom-cursor.png) no-repeat center center',
-                    pointerEvents: 'none',
-                    transform: 'translate(-50%, -50%)', // Center the cursor image
-                }}
             />
             <div className="flex flex-col items-center justify-center">
                 <div className="flex flex-col items-center justify-center h-[100%] w-[100%]">
-                    {/* <h1 className='font-rp1 text-4xl md:text-8xl text-white sparkHero'>SPARK</h1> */}
                     <img 
                         src="/assets/spark/wheel.gif" 
                         className='mobile1:scale-x-[-0.65] mobile1:scale-y-[0.65] sm:scale-x-[-0.5] sm:scale-y-[0.5] xl:scale-x-[-0.55] xl:scale-y-[0.55] scale-x-[-1] scale-y-[1] -translate-y-10 mobile1:-translate-y-15 md:-translate-y-20' 
