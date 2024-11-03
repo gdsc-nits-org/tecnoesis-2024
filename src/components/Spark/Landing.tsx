857d0ffa3985a36ea74ec77763f73d5016a9eaf2
barnilsarma <barnilsarma@gmail.com>
2024-11-03 17:46:35 +0530

    add module head

--- src/components/Spark/Landing.tsx
+++ src/components/Spark/Landing.tsx
@@ -1,6 +1,5 @@
 "use client";
 import React from 'react';
-import CustomCursor from './CustomCursor';
 const Landing: React.FC = () => {
   return (
     <div className="sparkheroParent scale-x-[-1] text-white w-[100vw] min-h-[50vh] md:min-h-[100vh] bg-[url('/assets/spark/sparklanding.png')] bg-cover bg-center flex flex-col justify-center items-center">
@@ -10,7 +9,6 @@ const Landing: React.FC = () => {
           <img src="/assets/spark/wheel.gif" className='mobile1:scale-x-[-0.65] mobile1:scale-y-[0.65] sm:scale-x-[-0.5] sm:scale-y-[0.5] xl:scale-x-[-0.55] xl:scale-y-[0.55] scale-x-[-1] scale-y-[1] -translate-y-10 mobile1:-translate-y-15 md:-translate-y-20' alt='wheel' />
         </div>
       </div>
-      <CustomCursor/>
     </div>
   );
 };
