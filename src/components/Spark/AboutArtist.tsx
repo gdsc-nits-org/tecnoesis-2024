97ddf938ffae4505fea00ac3e9001b347f95c16f
rishinhk004 <rishirajsaha555@gmail.com>
2024-11-04 12:44:06 +0530

    ui fix

--- src/components/Spark/AboutArtist.tsx
+++ src/components/Spark/AboutArtist.tsx
@@ -32,7 +32,7 @@ const AboutArtist = () => {
     return (
         <div className="w-full md:h-[60rem] bg-contain lg:bg-[length:80%] bg-center bg-no-repeat bg-[url('/assets/spark/sparkmobbg.png')] lg:bg-[url('/assets/spark/meettheartist.png')] flex flex-col">
 
-            <div className="h-fit w-full flex items-center justify-center translate-x-4 xl:translate-x-36  lg:translate-y-80 z-40">
+            <div className="h-fit w-full flex items-center justify-center translate-x-4 lg:translate-x-16 xl:translate-x-36  lg:translate-y-80 z-40">
                 <Image
                     src="/assets/spark/NikhilDSouza.png"
                     alt="nikhildsouza"
@@ -47,7 +47,7 @@ const AboutArtist = () => {
                     width={250}
                     className="lg:hidden translate-y-40 md:scale-[2] md:translate-y-[24rem]"
                 />
-                <div className="hidden lg:flex h-[5rem] w-[15rem] rounded-full -translate-x-32 bg-transparent border-white border-[.5px] -translate-y-[27rem]">
+                <div className="hidden xl:flex h-[5rem] w-[15rem] rounded-full -translate-x-32 bg-transparent border-white border-[.5px] -translate-y-[27rem]">
                     <div className={`h-full w-full rounded-full bg-[#19DCED] ${isPlaying ? '-translate-y-0' : '-translate-y-1/2'} flex items-center justify-center`}>
                         <div
                             onClick={handleClick}
@@ -69,7 +69,7 @@ const AboutArtist = () => {
             </div>
             <div className="w-full flex items-center justify-center">
                 <div className="h-[10rem] md:h-[12rem] lg:h-[15rem] w-[18rem] md:w-[25rem] glassmorphism z-50 
-            translate-y-16  lg:-translate-x-96 lg:-translate-y-8 xl:-translate-y-0 md:translate-y-[26rem] flex flex-col items-center justify-center" >
+            translate-y-16 lg:-translate-x-40 xl:-translate-x-96 lg:-translate-y-8 xl:-translate-y-0 md:translate-y-[26rem] flex flex-col items-center justify-center" >
                     <p className="text-white font-rp1 font-semibold text-center tracking-widest text-xl mb-2">Nikhil D Souza</p>
                     <p className="text-white font-outfit text-justify text-sm w-[90%] xl:text-md">Nikhil D Souza is an Indian singer-songwriter known for his soulful voice and heartfelt lyrics, blending pop, indie, and folk. With hits like Silver and Gold, he’s popular in India and internationally.</p>
                 </div>
